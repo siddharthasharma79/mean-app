8621629187a21de3026a91fa5a9d13366927ac03
Siddhartha Sharma <siddharthasharma79@gmail.com>
2019-09-12 07:31:55 +0530

    Angular UI completed

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: 'First Post', content: 'This is first post content!' },
+  //   { title: 'Second Post', content: 'This is second post content!' },
+  //   { title: 'Third Post', content: 'This is third post content!' }
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
